9e593f37acdff1da6d485a4382a6905993ee5deb
Donnycee <dmwendia30@gmail.com>
2026-04-05 15:58:30 +0300

    railway config settings

--- reset-password.js
+++ reset-password.js
@@ -1,6 +1,11 @@
-// reset-password.js
 import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
 
-const newPassword = 'Admin@123';
-const hash = await bcrypt.hash(newPassword, 12);
-console.log('New hash:', hash);
+const prisma = new PrismaClient();
+const hash = await bcrypt.hash('Admin123!', 10);
+await prisma.user.update({
+  where: { email: 'admin@campaign.com' },
+  data: { password: hash }
+});
+console.log('Password updated successfully!');
+await prisma.$disconnect();
